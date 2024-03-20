ed77e1f2900b5c11188b8ac21c08102137d2b424
Eldor <eldorturgunov777@gmail.com>
2024-03-20 11:41:37 +0500

    latest

--- lib/presentation/location_screen/location_screen.dart
+++ lib/presentation/location_screen/location_screen.dart
@@ -9,18 +9,107 @@ import 'dart:ui';
 import 'package:doctor/services/yandex_map_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 
 import 'controller/location_controller.dart';
 
-class LocationScreen extends StatefulWidget {
-  LocationScreen({super.key});
+class LocationScreen extends StatelessWidget {
+  const LocationScreen({super.key});
 
+  // final mapControllerCompleter = Completer<YandexMapController>();
   @override
-  State<LocationScreen> createState() => _LocationScreenState();
+  Widget build(BuildContext context) {
+    //width and height of the screen
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        flexibleSpace: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: Container(
+              color: Colors.white.withAlpha(100),
+            ),
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white.withAlpha(100),
+        title: const Text('Doctor Details'),
+        centerTitle: true,
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(200),
+              child: Container(
+                padding: const EdgeInsets.all(40),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor.withOpacity(0.2),
+                ),
+                child: Image.asset('assets/images/location.png'),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Location',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Please enable location permission to see the doctor location',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            //button open location
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).primaryColor,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                Get.to(() => const OpenLocation());
+              },
+              child: const Text(
+                'Open Location',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _LocationScreenState extends State<LocationScreen>
+class OpenLocation extends StatefulWidget {
+  const OpenLocation({super.key});
+
+  @override
+  State<OpenLocation> createState() => _OpenLocationState();
+}
+
+class _OpenLocationState extends State<OpenLocation>
     with AutomaticKeepAliveClientMixin {
   final mapControllerCompleter = Completer<YandexMapController>();
   final _locationController = Get.put(LocationController());
@@ -34,7 +123,7 @@ class _LocationScreenState extends State<LocationScreen>
             latitude: appLatLong.lat,
             longitude: appLatLong.long,
           ),
-          zoom: 14,
+          zoom: 12,
         ),
       ),
     );
@@ -42,14 +131,18 @@ class _LocationScreenState extends State<LocationScreen>
 
   @override
   bool get wantKeepAlive => true;
-
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    //width and height of the screen
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Iconsax.arrow_left_2),
+          onPressed: () {
+            Get.back();
+          },
+        ),
         flexibleSpace: ClipRect(
           child: BackdropFilter(
             filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
